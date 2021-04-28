e806c172105a8143b335a24ef23474b683cf9fa6
John <exarhouleasjohn@gmail.com>
2021-04-28 19:46:48 -0400

    Fixed Box bug that allows 2 of the same to be full

--- frontend vanillajs/src/gridboxes.js
+++ frontend vanillajs/src/gridboxes.js
@@ -8,15 +8,22 @@ function getRandomBoxNumber() {
 
 function clicked(event) {
     event.preventDefault()
-    console.log(event.target.id)
-    console.log(fullBoxes)
+    // console.log(event.target.id)
+    // console.log(fullBoxes)
 
     if (fullBoxes.includes(parseInt(event.target.id))) {
+        
+        let newFullBox = getRandomBoxNumber()
+        while (fullBoxes.includes(newFullBox)){
+            newFullBox = getRandomBoxNumber()
+        }
+
         fullBoxes = fullBoxes.filter(box => box != (event.target.id))
         changeColor(event.target, colorTheme[2])
         score.value = parseInt(score.value) + 100
 
-        
+        fullBoxes.push(newFullBox)
+        document.getElementById(newFullBox).style.backgroundColor = colorTheme[0]
     } else {
         gameEnd()
     }
